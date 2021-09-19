40c9d5d8bc13568ec776e0c3ecec52f7b1234623
Alexandr <XoxlofAlexandr@yandex.ru>
2021-09-19 23:31:40 +0300

    добавлена постраничная навигация

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import { Redirect, Route, Switch } from "react-router";
+import { privateRoutes, publicRoutes, RouteNames } from "../router/intex";
+
+const AppRouter: FC = () => {
+  const auth = true;
+  return auth ? (
+    <Switch>
+      {privateRoutes.map((route) => {
+        return (
+          <Route
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+            key={route.path}
+          />
+        );
+      })}
+      <Redirect to={RouteNames.EVENT} />
+    </Switch>
+  ) : (
+    <Switch>
+      {publicRoutes.map((route) => {
+        return (
+          <Route
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+            key={route.path}
+          />
+        );
+      })}
+      <Redirect to={RouteNames.LOGIN} />
+    </Switch>
+  );
+};
+
+export default AppRouter;
